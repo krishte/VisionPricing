5f3592f6321463f451c931c6fbaaedd46762b540
tejas.krishnan81 <tejas.krishnan81@gmail.com>
2024-04-25 07:07:26 -0400

    added logic for multiple products in single image

--- classifier.py
+++ classifier.py
@@ -26,12 +26,12 @@ import matplotlib.pyplot as plt
 
 ### Pipeline for using models
 
-image_path = "test_images/fridge_1.png"
+image_path = "test_images/logi_test.png"
 
 object_indices = {72.: "refrigerator", 62.: "tv", 63.: "laptop", 64.: "mouse", 65.: "remote", 66.: "keyboard", 67.: "cell phone", 68.: "microwave", 69.: "oven", 70: "toaster", 78: "hair drier"}
 brand_indices = {0: 'KitchenAid', 1: 'LG', 2: 'Samsung', 3: 'Whirlpool', 4: 'Toshiba', 5: 'Miele', 6: 'Logitech', 7: 'Panasonic', 8: 'Electrolux', 9: 'Sony', 10: 'Morphy Richards', 11: 'Sharp', 12: 'Philips', 13: 'Oppo', 14: 'MSI', 15: 'IFB', 16: 'Huawei', 17: 'Havells', 18: 'Haier', 19: 'Acer'}
 object_specific_models = {"refrigerator": "runs/classify/train3/weights/best.pt", "mouse":"runs/classify/mouse_wired/weights/best.pt" }
-product_description = {"product" : "", "brand": "", "type": "", "color": ""}
+products = []
 
 rgb_to_text = [("black", (0,0,0)), ("white", (255, 255,255)),
                ("red", (255,0,0)), ("lime", (0,255, 0)),
@@ -50,55 +50,90 @@ object_results = object_model(image_path)
 #object_frame = object_results[0].plot()
 
 classes_found = list(object_results[0].boxes.cls)
+image = cv2.imread(image_path)
+object_frame = cv2.imread(image_path)
+
+#Whether the intersection area of two boxes is > 0.3 of the area of either box
+def intersecting_box(box1, box2):
+    intersect_box = [max(box1[0], box2[0]), max(box1[1], box2[1]), min(box1[2], box2[2]), min(box1[3], box2[3])]
+    area_intersect_box = (intersect_box[3]-intersect_box[1])*(intersect_box[2]-intersect_box[0])
+    area_box1 = (box1[3]-box1[1])*(box1[2]-box1[0])
+    area_box2 = (box2[3] - box2[1])*(box2[2]-box2[0])
+    return area_intersect_box > 0.5*area_box1 and area_intersect_box > 0.5*area_box2
 
 for index, item in object_indices.items():
+    #select largest area box for overlapping boxes and corresponding confidence score
     if index in classes_found:
-        #Cropping the image to just the product and setting product
-        product_description["product"] = item
-        box = np.array(object_results[0].boxes.xyxy)[classes_found.index(index)]
-        prob = np.array(object_results[0].boxes.conf)[classes_found.index(index)]
-        image = cv2.imread(image_path)
-        # Blue border for object detected
-        object_frame = cv2.rectangle(image, (int(box[0]), int(box[1])), (int(box[2]), int(box[3])), (255, 0, 0), thickness=2) 
-        # Blue background for object label
-        object_frame = cv2.rectangle(object_frame, (int(box[0]), int(box[3])), (int(box[0]+300), int(box[3])-30) , (255, 0, 0), -1)
-        # Object label
-        object_frame = cv2.putText(object_frame, item + ": " + format(prob, ".2f")  , (int(box[0]), int(box[3])-5), cv2.FONT_HERSHEY_SIMPLEX ,  1, (255, 255, 255), 2) 
-        cropped_image =  image[int(box[1]):int(box[3]), int(box[0]):int(box[2])]
+        #boxes and confidences for all objects with the right index
+        classes_found_indices = [i for (i, val) in enumerate(np.array(classes_found)) if val==index]
+        boxes = np.array(object_results[0].boxes.xyxy)[classes_found_indices]
+        probs = np.array(object_results[0].boxes.conf)[classes_found_indices]
+        nonintersecting_boxes, nonintersecting_probs = [], []
 
-        # checking if there is an object specific model and setting type using cropped image
-        if item in object_specific_models:
-            object_specific_model = YOLO(object_specific_models[item])
-            object_specific_results = object_specific_model(cropped_image)
-            # Black background for classes
-            object_frame = cv2.rectangle(object_frame, (0,0), (250, 30+25*len(np.array(object_specific_results[0].probs.data))) , (0, 0, 0), -1)
-            object_frame = object_specific_results[0].plot(img=object_frame)
-            product_description['type'] = " ".join(object_specific_results[0].names[np.argmax(np.array(object_specific_results[0].probs.data))].split('_'))
-        
-        
-        # Finding the brand of the product
-        brand_results = brand_model(image)
-        object_frame = brand_results[0].plot(img=object_frame)
-        for brand_index in list(brand_results[0].boxes.cls):
-            product_description['brand'] = brand_indices[int(brand_index)]
-            break
+        for j,val in enumerate(boxes):
+            found = False
+            for i,box in enumerate(nonintersecting_boxes):
+                if intersecting_box(box, val):
+                    found = True
+                    if probs[j] > nonintersecting_probs[i]:
+                        nonintersecting_boxes[i] = val
+                        nonintersecting_probs[i] = probs[j]
+                        break
+            if not found:
+                nonintersecting_boxes.append(val)
+                nonintersecting_probs.append(probs[j])
+
+        for box, prob in zip(nonintersecting_boxes, nonintersecting_probs):
+            product_description = {"product" : item, "brand": "", "type": "", "color": ""}
+
+            # Blue border for object detected
+            object_frame = cv2.rectangle(object_frame, (int(box[0]), int(box[1])), (int(box[2]), int(box[3])), (255, 0, 0), thickness=5) 
+            # Blue background for object label
+            object_frame = cv2.rectangle(object_frame, (int(box[0]), int(box[3])), (int(box[0]+1200), int(box[3])-90) , (255, 0, 0), -1)
+            # Object label
+            object_frame = cv2.putText(object_frame, item + ": " + format(prob, ".2f")  , (int(box[0]), int(box[3])-5), cv2.FONT_HERSHEY_SIMPLEX ,  3, (255, 255, 255), 5) 
+            cropped_image =  image[int(box[1]):int(box[3]), int(box[0]):int(box[2])]
+
+            # checking if there is an object specific model and setting type using cropped image
+            if item in object_specific_models:
+                object_specific_model = YOLO(object_specific_models[item])
+                object_specific_results = object_specific_model(cropped_image)
+                # Black background for classes
+                #object_frame = cv2.rectangle(object_frame, (0,0), (250, 30+25*len(np.array(object_specific_results[0].probs.data))) , (0, 0, 0), -1)
+                #object_frame = object_specific_results[0].plot(img=object_frame)
+                product_description['type'] = " ".join(object_specific_results[0].names[np.argmax(np.array(object_specific_results[0].probs.data))].split('_'))
+            
+            
+            # Finding the brand of the product
+            brand_results = brand_model(cropped_image)
+            #object_frame = brand_results[0].plot(img=object_frame)
+            for brand_index in list(brand_results[0].boxes.cls):
+                product_description['brand'] = brand_indices[int(brand_index)]
+                logo_box = np.array(brand_results[0].boxes.xyxy)[0]
+                # Green border for logo detected
+                object_frame = cv2.rectangle(object_frame, (int(logo_box[0]+box[0]), int(logo_box[1]+box[1])), (int(logo_box[2]+box[0]), int(logo_box[3]+box[1])), (17, 120, 10), thickness=5) 
+                # Green background for logo label
+                object_frame = cv2.rectangle(object_frame, (int(logo_box[0]+box[0]), int(logo_box[1]+box[1])), (int(logo_box[0]+box[0]+1200), int(logo_box[1]+box[1]-90)) , (17, 120, 10), -1)
+                # logo label
+                object_frame = cv2.putText(object_frame, brand_indices[int(brand_index)] + ": " + format(np.array(brand_results[0].boxes.conf)[0], ".2f")  , (int(logo_box[0]+box[0]), int(logo_box[1]+box[1])-5), cv2.FONT_HERSHEY_SIMPLEX ,  3, (255, 255, 255), 5) 
+                break
 
 
 
-        # Finding the color of the product
-        cropped_image_rgb = cv2.cvtColor(cropped_image, cv2.COLOR_BGR2RGB)
-        reshape = cropped_image_rgb.reshape((cropped_image_rgb.shape[0] * cropped_image_rgb.shape[1], 3))
+            # Finding the color of the product
+            cropped_image_rgb = cv2.cvtColor(cropped_image, cv2.COLOR_BGR2RGB)
+            reshape = cropped_image_rgb.reshape((cropped_image_rgb.shape[0] * cropped_image_rgb.shape[1], 3))
 
-        cluster = KMeans(n_clusters=5).fit(reshape)
+            cluster = KMeans(n_clusters=3).fit(reshape)
 
-        color = np.array(cluster.cluster_centers_[mode(cluster.labels_)])
-        print(color)
-        product_description['color'] = min([(np.linalg.norm(np.array(col)-color), name) for (name, col) in rgb_to_text])[1]
-
+            color = np.array(cluster.cluster_centers_[mode(cluster.labels_)])
+            print(color)
+            product_description['color'] = min([(np.linalg.norm(np.array(col)-color), name) for (name, col) in rgb_to_text])[1]
+            products.append(product_description)
 
 
         cv2.imwrite("test.png", object_frame)
 
-        print(product_description)
+        print(products)
         break
 
--- labelEditer.py
+++ labelEditer.py
@@ -11,7 +11,7 @@ import numpy as np
 
 ### Shift all the label values in a YOLO dataset
 
-# path = "labels"
+# path = "cringe_better"
 # onlyfiles = [f for f in listdir(path) if isfile(join(path, f))]
 
 # for filename in onlyfiles:
@@ -29,35 +29,35 @@ import numpy as np
 
 ### Data preprocessing: cropping object images to object
 
-types = ["4-slice", "2-slice"]
-not_found = []
-object_index = 70.
-item = "toaster"
-dataset = "toaster_type"
+# types = ["4-slice", "2-slice"]
+# not_found = []
+# object_index = 70.
+# item = "toaster"
+# dataset = "toaster_type"
 
-for fridge_type in types:
-    path = "datasets/" + dataset + "/" + fridge_type
-    onlyfiles = [f for f in listdir(path) if isfile(join(path, f))]
+# for fridge_type in types:
+#     path = "datasets/" + dataset + "/" + fridge_type
+#     onlyfiles = [f for f in listdir(path) if isfile(join(path, f))]
 
-    object_model = YOLO('runs/detect/train/weights/best.pt')
+#     object_model = YOLO('runs/detect/train/weights/best.pt')
 
 
-    for i,file in enumerate(onlyfiles):
-        image_path = path + "/" + file
-        object_results = object_model(image_path)
+#     for i,file in enumerate(onlyfiles):
+#         image_path = path + "/" + file
+#         object_results = object_model(image_path)
 
-        try:
-            print(object_results[0].boxes.cls)
-            box =  np.array(object_results[0].boxes.xyxy)[list(object_results[0].boxes.cls).index(object_index)]
-        except:
-            print("no " + item + " found in image " + file)
-            not_found.append(fridge_type + " " + file)
-            continue
+#         try:
+#             print(object_results[0].boxes.cls)
+#             box =  np.array(object_results[0].boxes.xyxy)[list(object_results[0].boxes.cls).index(object_index)]
+#         except:
+#             print("no " + item + " found in image " + file)
+#             not_found.append(fridge_type + " " + file)
+#             continue
 
-        image = cv2.imread(image_path)
-        cv2.imwrite("datasets/" + dataset + "_real/train/"+ fridge_type + "/" + fridge_type + "_" + str(i) +".png", image[int(box[1]):int(box[3]), int(box[0]):int(box[2])])
+#         image = cv2.imread(image_path)
+#         cv2.imwrite("datasets/" + dataset + "_real/train/"+ fridge_type + "/" + fridge_type + "_" + str(i) +".png", image[int(box[1]):int(box[3]), int(box[0]):int(box[2])])
 
-print(not_found)
+# print(not_found)
 
 ### Print a dictionary of label numbers to label strings from yaml
             
@@ -67,14 +67,14 @@ print(not_found)
 
 ### Convert webp to png 
 
-# path = "images"
+path = "images"
 
 # only_folders = [f for f in listdir(path)]
 
 # for folder in only_folders:
-#     only_files = [f for f in listdir(path + "/" + folder)]
-#     for filename in only_files:
-#         if (filename.split('.')[-1] == 'webp'):
-#             im = Image.open(path + "/" + folder + "/" + filename).convert('RGB')
-#             im.save(path + '/' + folder + '/' + filename.split('.')[0] + ".png", "png")
-#             os.remove(path + "/" + folder + "/" + filename)
+only_files = [f for f in listdir(path)]
+for filename in only_files:
+    if (filename.split('.')[-1] == 'webp'):
+        im = Image.open(path + "/" + filename).convert('RGB')
+        im.save(path + '/' + filename.split('.')[0] + ".png", "png")
+        os.remove(path + "/" + filename)
